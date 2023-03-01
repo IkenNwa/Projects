c2d82035c7b7ad7b7a586437d4667a893854c5b0
Ikechukwu Nwachukwu <cfingers1chukwu@gmail.com>
2023-03-01 18:48:58 +0000

    Fixed API request Issue

--- moviesReact/src/components/Search.jsx
+++ moviesReact/src/components/Search.jsx
@@ -1,19 +1,27 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 
 function Search() {
+  const apiKey = "7ef821b54fabad2e9b24bac552a96002";
 
-  const [post, setPost] = useState(null)
-function handleClick() {
+
+  const [post, setPost] = useState();
+
+  useEffect(() => {
     axios
-    .get(
-      "https://api.themoviedb.org/3/movie/201?api_key=7ef821b54fabad2e9b24bac552a96002"
-    )
-    .then((res) => {
-      console.log(res.data);
-       setPost(res.data)
-    });
-}
+  .get(
+    // `https://api.themoviedb.org/3/movie/${Math.floor(Math.random()*1000)}?api_key=${apiKey}`
+    `https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}&language=en-US&page=1`
+  )
+  .then((res) => {
+    console.log(res.data.results);
+    setPost(res.data.results);
+    console.log(post);
+  });
+  },[]);
+  function handleClick() {
+      console.log(post);
+  }
 
   return (
     <div>
@@ -21,9 +29,20 @@ function handleClick() {
       <button onClick={handleClick}>Search</button>
 
       <div>
-        <img src={`https://image.tmdb.org/t/p/w200${post ? post.poster_path : null}`} alt="Broken Image" />
-        <h2>{ post ? post.title : "Movie Title"}</h2>
-        <p>{ post ? post.overview : "Movie Desc"}</p>
+        {post
+          ? post.map((pos) => {
+              <div className="container">
+                <img
+                  src={`https://image.tmdb.org/t/p/w500${
+                    pos ? pos.poster_path : null
+                  }`}
+                  alt="Broken Image"
+                />
+                <h2>{pos.title}</h2>
+                <p>{pos.overview}</p>
+              </div>;
+            })
+          : ""}
       </div>
     </div>
   );
